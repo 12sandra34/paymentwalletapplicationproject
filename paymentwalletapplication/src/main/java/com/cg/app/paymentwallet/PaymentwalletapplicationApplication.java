2b1b27bc2c33d174b9d1d5e08a45d2169d31da5a
12sandra34 <sandra.ke.sreedhar@gmail.com>
2022-01-06 12:34:52 +0530

    first commit

---
+++ paymentwalletapplication/src/main/java/com/cg/app/paymentwallet/PaymentwalletapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.cg.app.paymentwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentwalletapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentwalletapplicationApplication.class, args);
+	}
+
+}
